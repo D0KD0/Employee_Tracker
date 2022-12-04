0d68dc79f35d4e23ab8c1ab7fe9dcfa73cea13e7
D0KD0 <113081917+D0KD0@users.noreply.github.com>
2022-12-03 20:54:30 -0800

    server.js updated

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 GIVEN a command-line application that accepts user input
 
-WHEN I start the application
-THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+/ WHEN I start the application
+THEN I am presented with the following options: 
+0 view all departments, 
+1 view all roles, 
+2 view all employees, 
+3 add a department, 
+4 add a role, 
+5 add an employee, 
+6 and update an employee role
 
 WHEN I choose to view all departments
 THEN I am presented with a formatted table showing department names and department ids
@@ -14,14 +21,14 @@ THEN I am presented with the job title, role id, the department that role belong
 WHEN I choose to view all employees
 THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
-WHEN I choose to add a department
+/ WHEN I choose to add a department
 THEN I am prompted to enter the name of the department and that department is added to the database
 
-WHEN I choose to add a role
+/ WHEN I choose to add a role
 THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
-WHEN I choose to add an employee
+/ WHEN I choose to add an employee
 THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
-WHEN I choose to update an employee role
+/ WHEN I choose to update an employee role
 THEN I am prompted to select an employee to update and their new role and this information is updated in the database
--- server.js
+++ server.js
@@ -1,53 +1,20 @@
 // get the client
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
-// from website
-    // create the connection to database
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'test'
-    });
+const add = require('./js/add');
+const view = require('./js/view');
+const update = require('./js/update');
 
-    // simple query
-    connection.query(
-      'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-      function(err, results, fields) {
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-      }
-    );
-
-    // with placeholder
-    connection.query(
-      'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-      ['Page', 45],
-      function(err, results) {
-        console.log(results);
-      }
-    );
-
-    import inquirer from 'inquirer';
-
-    inquirer
-      .prompt([
-        /* Pass your questions in here */
-      ])
-      .then((answers) => {
-        // Use user feedback for... whatever!!
-      })
-      .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else went wrong
-        }
-      });
-
-
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test',
+  password: ''
+});
 
 // start of question 
-const questions = [
+const firstQuestion = [
   {
     type: 'input',
     message: 'Choose one of the following options:',
@@ -62,7 +29,225 @@ const questions = [
       'Update an employee role'
     ]
   }
-    
-
-
 ]
+
+const addDeptQuestions = [
+  {
+    type: 'input',
+    message: 'Enter a name of the department to add',
+    name: 'deptName'
+  }
+]
+
+const decideDept = () => {
+  db.query(`select * from department order by id`, function (err, results) {
+    results.forEach(dep => {
+      addDeptQuestions[2].choices.push(dep.name);
+    });
+  })
+}
+
+const addRoleQuestions = [
+  {
+    type: 'input',
+    message: 'Enter a name of the role',
+    name: 'roleName'
+  },
+
+  {
+    type: 'input',
+    message: 'Enter a salary of the role',
+    name: 'roleSalary'
+  },
+
+  {
+    type: 'input',
+    message: 'Enter a department of the role',
+    name: 'roleDept'
+  }
+]
+
+const decideRole = () => {
+  db.query(`select * from role order by id`, function (err, results) {
+    results.forEach(role => {
+      addRoleQuestions[2].choices.push(dep.name);
+    });
+  })
+}
+
+const addEmployeeQuestions = [
+  {
+    type: 'input',
+    message: 'Enter a first name of the employee',
+    name: 'firstName'
+  },
+
+  {
+    type: 'input',
+    message: 'Enter a last name of the employee',
+    name: 'lastName'
+  },
+
+  {
+    type: 'input',
+    message: 'Enter a role of the employee',
+    name: 'newEmpRole'
+  },
+
+  {
+    type: 'input',
+    message: 'What is a name of his/her Manager?',
+    name: 'newEmpMgr',
+    choices: ['Unknown']
+  }
+]
+
+const decideMgr = () => {
+  db.query(`SELECT id, first_name FROM employee order by id`, function (err, results) {
+      results.forEach(employee => {
+          addEmployeeQuestions[3].choices.push(employee.first_name);
+          updateEmpQuestions[0].choices.push(employee.first_name);
+      });
+    })
+    
+}
+
+const updateEmpQuestions = [
+  {
+    type: 'list',
+    message: 'Which employee you want to update his/her role?',
+    name: 'empName',
+    choices: []
+  },
+
+  {
+      type : 'list',
+      message : 'Which role do you want to assign to the employee?',
+      name : 'addRole',
+      choices : []
+  }
+]
+
+const updateEmpChoices = () => {
+  db.query(`SELECT id, first_name FROM employee`, function (err, results) {
+      results.forEach(employee => {
+          updateEmpQuestions[0].choices.push(employee.first_name);
+          // console.log(updateEmpQuestions[0].choices)
+      });
+    })
+    
+}
+
+const updateRoleChoices = () => {
+  db.query(`SELECT id, title FROM role`, function (err, results) {
+      results.forEach(role => {
+          updateEmpQuestions[1].choices.push(role.title);
+          // console.log(updateEmployeeQuestions[1].choices)
+      });
+    })
+    
+}
+
+// choices
+
+const {choices} = firstQuestion[0];
+const startPrompt = () => {
+  inquirer
+  .prompt(firstQuestion)
+  .then( (choiceObj) => {
+    const {choice} = choiceObj;
+    switch(choice) {
+      case choices[0] :
+      view.viewDepts();
+      startPrompt();
+      break;
+
+      case choices[1] :
+      view.viewRoles();
+      startPrompt();
+      break;
+
+      case choices[2] :
+      view.viewEmployees();
+      startPrompt();
+      break;
+
+      case choices[3] :
+      addDeptPrompt();
+      break;
+
+      case choices[4] :
+      decideDept();
+      addRolePrompt();
+      break;
+
+      case choices[5] :
+      decideDept();
+      decideRole();
+      decideMgr();
+      addEmpPrompt();
+      break;
+
+      case choices[6] :
+      updateEmpChoices();
+      updateRoleChoices();
+      updateEmpPrompt();
+      break;
+
+      case choice[7] :
+      break;
+    }
+  })
+}
+
+// prompt
+const addDeptPrompt = () => {
+  inquirer.prompt(addDeptQuestions)
+  .then((deptObj) => {
+    add.addDept(deptObj.depName);
+  })
+  .then(() => startPrompt())
+}
+
+const addRolePrompt = () => {
+  inquirer.prompt(addRoleQuestions)
+  .then((roleObj) => {
+    const { roleName, roleSalary, roleDep } = roleObj;
+    db.query(`select id from department where name = ?`, roleDep, function(err, results) {
+      const roleIndex = results[0].id;
+      add.addRole(roleName, roleSalary, roleIndex);
+  })
+})
+  .then(()=> startPrompt())
+}
+
+const addEmpPrompt = () => {
+  inquirer.prompt(addEmployeeQuestions)
+          .then((empObj) => {
+              const { firstname, lastname, role, manager } = empObj;
+              db.query(`select role.id as roleId from role where title = ?`, role, function(err, results) {
+                  const roleIndex = results[0].roleId
+              db.query(`select employee.id as managerId from employee where first_name = ?`, manager, function(err, results) {
+                  const managerIndex = results[0].managerId
+                  add.addEmployees(firstname, lastname, roleIndex, managerIndex)
+              })
+          })
+
+          })
+          .then(()=> startPrompt())
+}
+
+
+const updateEmpPrompt = () => {
+  inquirer.prompt(updateEmpQuestions)
+          .then((updateObj) => {
+              const { employee, role } = updateObj;
+              db.query(`select id from role where title = ?`, role, function(err, results) {
+                  const roleIndex = results[0].id;
+                  update(roleIndex, employee);
+              })
+          })
+          .then(()=> startPrompt())
+}
+
+startPrompt();
